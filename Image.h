8a95712db428bafd3038533ddc2b0cd7e0594a72
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-09-25 14:35:56 +0300

    Add zero-padded convolution with some filters.

--- Image.h
+++ Image.h
@@ -16,25 +16,26 @@ enum ImageType {
 
 
 struct Image {
-    uint8_t* data = nullptr;
+    uint8_t *data = nullptr;
     size_t size = 0;
     int w;
     int h;
     int channels;
 
-    explicit Image(const char* filename);
+    explicit Image(const char *filename);
     Image(int w, int h, int channels);
-    Image(const Image& img);
+    Image(const Image &img);
     ~Image();
 
-    bool read(const char* filename);
-    bool write (const char* filename);
+    bool read(const char *filename);
+    bool write (const char *filename);
 
-    ImageType getFileType(const char* filename);
+    ImageType getFileType(const char *filename);
 
-    Image& grayscaleAvg();
-    Image& grayscaleLum();
-    Image& colorMask(float r, float g, float b);
+    Image &grayscaleAvg();
+    Image &grayscaleLum();
+    Image &colorMask(float r, float g, float b);
+    Image &stdConvolveClampTo0(uint8_t channel, uint32_t ker_w, uint32_t ker_h, const double *ker, uint32_t cr, uint32_t cc);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,11 +3,46 @@
 #include "Image.h"
 
 int main() {
-    Image test("/Users/mishashkarubski/CLionProjects/Fourier/test.jpg");
+    Image test("/Users/mishashkarubski/CLionProjects/Fourier/lenna.png");
 
-    test.colorMask(0.86, 1, 0.4);
+    double emboss[] = {
+            -2 / 9.0, -1 / 9.0, 0.0,
+            -1 / 9.0, 1 / 9.0, 1 / 9.0,
+            0.0, 1 / 9.0, 2 / 9.0
+    };
+    double gaussian[] = {
+            1 / 273.0, 4 / 273.0, 7 / 273.0, 4 / 273.0, 1 / 273.0,
+            4 / 273.0, 16 / 273.0, 26 / 273.0, 16 / 273.0, 4 / 273.0,
+            7 / 273.0, 26 / 273.0, 41 / 273.0, 26 / 273.0, 7 / 273.0,
+            4 / 273.0, 16 / 273.0, 26 / 273.0, 16 / 273.0, 4 / 273.0,
+            1 / 273.0, 4 / 273.0, 7 / 273.0, 4 / 273.0, 1 / 273.0,
+    };
+    double canny_h[] = {
+            -1, -2, -1,
+            0, 0, 0,
+            1, 2, 1
+    };
+    double canny_v[] = {
+            -1, 0, 1,
+            -2, 0, 2,
+            -1, 0, 1
+    };
 
-    test.write("/Users/mishashkarubski/CLionProjects/Fourier/test_greeny.png");
+    test.stdConvolveClampTo0(0, 3, 3, canny_v, 1, 1);
+    test.stdConvolveClampTo0(1, 3, 3, canny_v, 1, 1);
+    test.stdConvolveClampTo0(2, 3, 3, canny_v, 1, 1);
+
+    test.stdConvolveClampTo0(0, 3, 3, canny_h, 1, 1);
+    test.stdConvolveClampTo0(1, 3, 3, canny_h, 1, 1);
+    test.stdConvolveClampTo0(2, 3, 3, canny_h, 1, 1);
+
+    test.grayscaleLum();
+
+    test.stdConvolveClampTo0(0, 5, 5, gaussian, 2, 2);
+    test.stdConvolveClampTo0(1, 5, 5, gaussian, 2, 2);
+    test.stdConvolveClampTo0(2, 5, 5, gaussian, 2, 2);
+
+    test.write("/Users/mishashkarubski/CLionProjects/Fourier/filtered4.png");
 
     return 0;
 }
