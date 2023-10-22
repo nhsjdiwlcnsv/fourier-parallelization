96e6484dbd5fec094bcc16a650afb5c5cdb09129
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-10-22 15:10:54 +0300

    Fix FFT & IFFT for 2D vectors

--- main.cpp
+++ main.cpp
@@ -3,23 +3,29 @@
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <chrono>
 
-std::vector<std::vector<uint8_t>> Conv(const std::vector<std::vector<int>>& image, const std::vector<std::vector<int>>& kernel) {
+#define freqMat std::vector<std::vector<std::complex<double>>>
+#define freqVec std::vector<std::complex<double>>
+#define cmplx std::complex<double>
+
+template <class T, class U>
+std::vector<std::vector<T>> Conv(const std::vector<std::vector<T>>& image, const std::vector<std::vector<U>>& kernel) {
     size_t imgHeight = image.size();
     size_t imgWidth = image[0].size();
-    size_t kSize = kernel.size();
-    size_t kCenter = kSize / 2;
+    int kSize = kernel.size();
+    int kCenter = kSize / 2;
 
-    std::vector<std::vector<uint8_t>> result(imgHeight, std::vector<uint8_t>(imgWidth, 0));
+    std::vector<std::vector<T>> result(imgHeight, std::vector<T>(imgWidth, 0));
 
-    for (size_t i = kCenter; i < imgHeight - kCenter; ++i) {
-        for (size_t j = kCenter; j < imgWidth - kCenter; ++j) {
-            uint8_t sum = 0;
-            for (size_t k = -kCenter; k <= kCenter; ++k) {
-                for (size_t l = -kCenter; l <= kCenter; ++l) {
+    for (int i = kCenter; i < imgHeight - kCenter; ++i) {
+        for (int j = kCenter; j < imgWidth - kCenter; ++j) {
+            T sum = 0;
+
+            for (int k = -kCenter; k <= kCenter; ++k)
+                for (int l = -kCenter; l <= kCenter; ++l)
                     sum += image[i + k][j + l] * kernel[k + kCenter][l + kCenter];
-                }
-            }
+
             result[i][j] = sum;
         }
     }
@@ -28,25 +34,25 @@ std::vector<std::vector<uint8_t>> Conv(const std::vector<std::vector<int>>& imag
 }
 
 template <class T>
-std::vector<std::vector<std::complex<double>>> fft(const std::vector<std::vector<T>>& image) {
+freqMat fft2d(const std::vector<std::vector<T>>& image) {
     size_t imgHeight = image.size();
     size_t imgWidth = image[0].size();
 
     // Convert image to complex values
-    std::vector<std::vector<std::complex<double>>> imgTransformed(imgHeight, std::vector<std::complex<double>>(imgWidth));
+    freqMat imgTransformed(imgHeight, freqVec(imgWidth));
     for (size_t i = 0; i < imgHeight; ++i)
         for (size_t j = 0; j < imgWidth; ++j)
-            imgTransformed[i][j] = std::complex<double>(image[i][j], 0);
+            imgTransformed[i][j] = cmplx(image[i][j], 0);
 
-    // Apply fft to each row
+    // Apply fft2d to each row
     for (size_t i = 0; i < imgHeight; ++i) {
-        std::vector<std::complex<double>> row = imgTransformed[i];
-        std::vector<std::complex<double>> transformedRow(imgWidth);
+        freqVec row = imgTransformed[i];
+        freqVec transformedRow(imgWidth);
 
         for (size_t j = 0; j < imgWidth; ++j) {
-            std::complex<double> sum(0, 0);
+            cmplx sum(0, 0);
             for (size_t k = 0; k < imgWidth; ++k)
-                sum += row[k] * std::exp(std::complex<double>(0, -2 * M_PI * j * k / imgWidth));
+                sum += row[k] * std::exp(cmplx(0, -2 * M_PI * j * k / imgWidth));
 
             transformedRow[j] = sum;
         }
@@ -54,18 +60,18 @@ std::vector<std::vector<std::complex<double>>> fft(const std::vector<std::vector
         imgTransformed[i] = transformedRow;
     }
 
-    // Apply fft to each column
+    // Apply fft2d to each column
     for (size_t j = 0; j < imgWidth; ++j) {
-        std::vector<std::complex<double>> column(imgHeight);
-        std::vector<std::complex<double>> transformedColumn(imgHeight);
+        freqVec column(imgHeight);
+        freqVec transformedColumn(imgHeight);
 
         for (size_t i = 0; i < imgHeight; ++i)
             column[i] = imgTransformed[i][j];
 
         for (size_t i = 0; i < imgHeight; ++i) {
-            std::complex<double> sum(0, 0);
+            cmplx sum(0, 0);
             for (size_t k = 0; k < imgHeight; ++k)
-                sum += column[k] * std::exp(std::complex<double>(0, -2 * M_PI * i * k / imgHeight));
+                sum += column[k] * std::exp(cmplx(0, -2 * M_PI * i * k / imgHeight));
 
             transformedColumn[i] = sum;
         }
@@ -79,71 +85,135 @@ std::vector<std::vector<std::complex<double>>> fft(const std::vector<std::vector
 
 
 template <class T>
-std::vector<std::vector<T>> ifft(const std::vector<std::vector<std::complex<double>>>& image) {
+std::vector<std::vector<T>> ifft2d(const freqMat& image) {
     size_t imgHeight = image.size();
     size_t imgWidth = image[0].size();
 
-    std::vector<std::vector<T>> imgITransformed(imgHeight, std::vector<T>(imgWidth));
+    freqMat imgITransformed(imgHeight, freqVec(imgWidth));
+    std::vector<std::vector<T>> result(imgHeight, std::vector<T>(imgWidth));
 
-    // Apply ifft to each column
+    // Apply ifft2d to each column
     for (size_t j = 0; j < imgWidth; ++j) {
-        std::vector<std::complex<double>> column(imgHeight);
-        std::vector<std::complex<double>> columnITransformed(imgHeight);
+        freqVec column(imgHeight);
+        freqVec columnITransformed(imgHeight);
 
         for (size_t i = 0; i < imgHeight; ++i)
             column[i] = image[i][j];
 
         for (size_t i = 0; i < imgHeight; ++i) {
-            std::complex<double> sum(0, 0);
+            cmplx sum(0, 0);
             for (size_t k = 0; k < imgHeight; ++k)
-                sum += column[k] * std::exp(std::complex<double>(0, 2 * M_PI * i * k / imgHeight));
+                sum += column[k] * std::exp(cmplx(0, 2 * M_PI * i * k / imgHeight));
 
             columnITransformed[i] = sum / (double) imgHeight;
         }
 
         for (size_t i = 0; i < imgHeight; ++i)
-            imgITransformed[i][j] = static_cast<T>(std::real(columnITransformed[i]));
+            imgITransformed[i][j] = columnITransformed[i];
     }
 
-    // Apply ifft to each row
+    // Apply ifft2d to each row
     for (size_t i = 0; i < imgHeight; ++i) {
-        std::vector<std::complex<double>> row(imgWidth);
-        std::vector<std::complex<double>> rowITransformed(imgWidth);
-
-        for (size_t j = 0; j < imgWidth; ++j)
-            row[i] = image[i][j];
+        freqVec row = imgITransformed[i];
+        freqVec rowITransformed(imgWidth);
 
         for (size_t j = 0; j < imgWidth; ++j) {
-            std::complex<double> sum(0, 0);
+            cmplx sum(0, 0);
             for (size_t k = 0; k < imgWidth; ++k)
-                sum += row[k] * std::exp(std::complex<double>(0, 2 * M_PI * j * k / imgWidth));
+                sum += row[k] * std::exp(cmplx(0, 2 * M_PI * j * k / imgWidth));
 
             rowITransformed[j] = sum / (double) imgWidth;
         }
 
-        for (size_t j = 0; j < imgWidth; ++j)
-            imgITransformed[i][j] = static_cast<T>(std::real(rowITransformed[j]));
+        imgITransformed[i] = rowITransformed;
     }
 
-    return imgITransformed;
+    // Convert the values to real
+    for (size_t i = 0; i < imgWidth; ++i)
+        for (size_t j = 0; j < imgHeight; ++j)
+            result[i][j] = static_cast<T>(std::real(imgITransformed[i][j]));
+
+    return result;
 }
 
+template <class T, class U>
+std::vector<std::vector<T>> ftConv(const std::vector<std::vector<T>>& image, const std::vector<std::vector<U>>& kernel) {
+    size_t imgHeight = image.size();
+    size_t imgWidth = image[0].size();
+    int kSize = kernel.size();
+    int kCenter = kSize / 2;
+
+    freqMat imgTransformed = fft2d<T>(image);
+    freqMat kernelTransformed = fft2d<U>(kernel);
+    freqMat resultTransformed(imgHeight, freqVec(imgWidth));
+
+    for (size_t i = 0; i < imgHeight; ++i) {
+        for (size_t j = 0; j < imgWidth; ++j) {
+            cmplx sum(0, 0);
+
+            for (int k = 0; k < kSize; ++k) {
+                for (int l = 0; l < kSize; ++l) {
+                    size_t imgY = (i + kCenter - k + imgHeight) % imgHeight;
+                    size_t imgX = (j + kCenter - l + imgWidth) % imgWidth;
+
+                    if (imgY < 0 || imgY >= imgHeight || imgX < 0 || imgX >= imgWidth)
+                        continue;
+
+                    sum += imgTransformed[imgY][imgX] * kernelTransformed[k][l];
+                }
+            }
+
+            resultTransformed[i][j] = sum;
+        }
+    }
+
+    std::vector<std::vector<T>> result = ifft2d<T>(resultTransformed);
+
+    return result;
+}
 
 int main() {
-    std::vector<std::vector<uint8_t>> image = {{255, 0, 255},
-                                               {0, 255, 0},
-                                               {255, 0, 255}};
+    std::vector<std::vector<int>> image = {{1, 2, 3, 4, 5},
+                                           {6, 7, 8, 9, 10},
+                                           {11, 12, 13, 14, 15},
+                                           {16, 17, 18, 19, 20},
+                                           {21, 22, 23, 24, 25}};
     std::vector<std::vector<int>> kernel = {{-1, 0, 1},
-                                            {-2,  0, 2},
+                                            {-2, 0, 2},
                                             {-1, 0, 1}};
 
-    std::vector<std::vector<std::complex<double>>> transformed_image = fft<uint8_t>(image);
-    std::vector<std::vector<std::complex<double>>> transformed_kernel = fft<int>(kernel);
+    auto beg1 = std::chrono::high_resolution_clock::now();
+    auto result1 = Conv(image, kernel);
+    auto end1 = std::chrono::high_resolution_clock::now();
 
-    auto image_result = ifft<uint8_t>(transformed_image);
-    auto kernel_result = ifft<uint8_t>(transformed_kernel);
+    auto beg2 = std::chrono::high_resolution_clock::now();
+    auto result2 = ftConv(image, kernel);
+    auto end2 = std::chrono::high_resolution_clock::now();
 
-    for (const auto& row : image_result) {
+    auto nImage = ifft2d<int>(fft2d<int>(image));
+
+    for (const auto& row : nImage) {
+        for (auto pixel : row)
+            std::cout << pixel << " ";
+
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end1 - beg1).count() << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end2 - beg2).count() << std::endl;
+    std::cout << std::endl;
+
+    for (const auto& row : result1) {
+        for (auto pixel : row)
+            std::cout << (double) pixel << " ";
+
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    for (const auto& row : result2) {
         for (auto pixel : row)
             std::cout << (double) pixel << " ";
 
